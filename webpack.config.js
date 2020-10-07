6ef93ffb0d3fa1f08ae986e67dce459bb9b26f7d
G4br13l4 <gabolf93@gmail.com>
2020-10-07 09:17:08 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+//path identifies if you are on Windows, Mac, Linux
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer : {
+        port: 4000
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
